046615c2bc7621e0e3f21e78e811de94e88d0b68
Lari Strand <lmstrand@gmail.com>
2025-01-02 15:15:45 +0200

    Add plugin type to plugin_data on install

--- Koha/Plugin/Fi/KohaSuomi/01ValidateMobileUncheckMessageTransports.pm
+++ Koha/Plugin/Fi/KohaSuomi/01ValidateMobileUncheckMessageTransports.pm
@@ -123,6 +123,12 @@ sub configure {
 sub install() {
     my ( $self, $args ) = @_;
 
+    $self->store_data(
+            {
+                type => 'intranetUserJs',
+            }
+        );
+        
     return 1;
 }
 
